ad32af56b52a7d23262224281f0d2c8dcc5e3cc0
Sverre <sverre.frogner.haugen@gmail.com>
2023-05-23 10:04:01 +0200

    CheckStyle PathsAssetStoreParser class

--- src/main/java/edu/ntnu/idatt2001/paths/io/PathsAssetStoreParser.java
+++ src/main/java/edu/ntnu/idatt2001/paths/io/PathsAssetStoreParser.java
@@ -1,6 +1,8 @@
 package edu.ntnu.idatt2001.paths.io;
 
-import edu.ntnu.idatt2001.paths.asset.*;
+import edu.ntnu.idatt2001.paths.asset.AudioAsset;
+import edu.ntnu.idatt2001.paths.asset.ImageAsset;
+import edu.ntnu.idatt2001.paths.asset.PathsAssetStore;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -10,15 +12,17 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * The purpose of this class is to create a PathsAssetStore by reading a .pathsassets-file and the contents of
- * its directory. <br>
+ * The purpose of this class is to create a PathsAssetStore by reading a .pathsassets-file
+ * and the contents of its directory. <br>
  * <br>
  *
- * This class is mainly focused around two methods:
- * <code>parsePathsAssetStore</code> and <code>getErrorsFromLastParse</code>. The former is the actual parser method.
- * The latter returns a collection of errors that caused the last call to <code>parsePathsAssetStore</code> to fail.
- * Note that every time <code>parsePathsAssetStore</code> is called, the errors from the parsing-operation before
- * will be lost.
+ * <p>This class is mainly focused around two methods:
+ * <code>parsePathsAssetStore</code> and <code>getErrorsFromLastParse</code>.
+ * The former is the actual parser method.
+ * The latter returns a collection of errors that caused the last call to
+ * <code>parsePathsAssetStore</code> to fail.
+ * Note that every time <code>parsePathsAssetStore</code> is called,
+ * the errors from the parsing-operation before will be lost.
  */
 public class PathsAssetStoreParser {
 
@@ -29,17 +33,34 @@ public class PathsAssetStoreParser {
 
     private List<String> loadErrors = new ArrayList<>();
 
+    private static boolean isImageAsset(String path) {
+        for (String s : IMAGE_FILE_ENDINGS)
+            if (path.endsWith(s))
+                return true;
+        return false;
+    }
+
+    private static boolean isAudioAsset(String path) {
+        for (String s : AUDIO_FILE_ENDINGS)
+            if (path.endsWith(s))
+                return true;
+        return false;
+    }
 
     /**
      * Indicates whether any errors occurred during the last parse-operation.
-     * @return <code>true</code> if any errors occurred, or <code>false</code> if no errors occurred.
+     *
+     * @return <code>true</code> if any errors occurred, or <code>false</code>
+     * if no errors occurred.
      */
     public boolean errorsOccurredLastParse() {
         return !loadErrors.isEmpty();
     }
 
     /**
-     * Returns a collection of the errors that occurred during the last call to <code>parsePathsAssetStore</code>.
+     * Returns a collection of the errors that occurred during the last call to
+     * <code>parsePathsAssetStore</code>.
+     *
      * @return The errors from the last parse, or <code>null</code> if no errors occurred.
      */
     public String[] getErrorsFromLastParse() {
@@ -49,12 +70,15 @@ public class PathsAssetStoreParser {
             return null;
     }
 
-
     /**
-     * Attempts to collect all assets specified in <code>assetRegisterData</code> from the directory
-     * <code>assetDirectoryPath</code>, and stores them as Asset-objects in a PathsAssetStore.
-     * @param assetRegisterData Textual data in line-separated key-value format, telling us the key and URI/filepath
-     *                          of each asset we expect to find within the specified <code>assetDirectoryPath</code>.
+     * Attempts to collect all assets specified in <code>assetRegisterData</code>
+     * from the directory <code>assetDirectoryPath</code>, and stores them as
+     * Asset-objects in a PathsAssetStore.
+     *
+     * @param assetRegisterData  Textual data in line-separated key-value format,
+     *                           telling us the key and URI/filepath
+     *                           of each asset we expect to find within the specified
+     *                           <code>assetDirectoryPath</code>.
      * @param assetDirectoryPath The directory path where we expect to find the assets.
      * @return A PathsAssetStore containing the assets found in the parsing process.
      */
@@ -68,7 +92,7 @@ public class PathsAssetStoreParser {
         String[] lines = assetRegisterData.lines().toArray(String[]::new);
         for (int i = 0; i < lines.length; i++) {
             String[] fields = lines[i].split(DATA_SEPARATOR + "(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-            String key  = fields[0].substring(1, fields[0].length() - 1);
+            String key = fields[0].substring(1, fields[0].length() - 1);
             String path = fields[1].substring(1, fields[1].length() - 1);
 
 
@@ -87,33 +111,15 @@ public class PathsAssetStoreParser {
                 ImageAsset asset = parseImageAsset(path, i, key);
                 if (asset != null)
                     imageAssets.put(key, asset);
-            }
-
-            else if (isAudioAsset(path)) {
+            } else if (isAudioAsset(path)) {
                 AudioAsset asset = parseAudioAsset(path, i);
                 if (asset != null)
                     audioAssets.put(key, asset);
-            }
-
-            else
-                loadErrors.add("Unsupported file type of asset named " + key + " at line " + (i+1) + ": \"" + path + "\". Supported types are: " + SUPPORTED_FILE_ENDINGS);
+            } else
+                loadErrors.add("Unsupported file type of asset named " + key + " at line " + (i + 1) + ": \"" + path + "\". Supported types are: " + SUPPORTED_FILE_ENDINGS);
         }
 
         return new PathsAssetStore(imageAssets, audioAssets);
-    }
-
-    private static boolean isImageAsset(String path) {
-        for (String s : IMAGE_FILE_ENDINGS)
-            if (path.endsWith(s))
-                return true;
-        return false;
-    }
-
-    private static boolean isAudioAsset(String path) {
-        for (String s : AUDIO_FILE_ENDINGS)
-            if (path.endsWith(s))
-                return true;
-        return false;
     }
 
     private ImageAsset parseImageAsset(String URI, int line, String key) {
