118489cabd1136fef8cc9f079cfb9a0d330e6594
Sverre <sverre.frogner.haugen@gmail.com>
2023-05-23 09:23:02 +0200

    CheckStyle ScoreGoal class

--- src/main/java/edu/ntnu/idatt2001/paths/model/goal/ScoreGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/model/goal/ScoreGoal.java
@@ -3,23 +3,25 @@ package edu.ntnu.idatt2001.paths.model.goal;
 import edu.ntnu.idatt2001.paths.model.Player;
 
 /**
- * Used to check if a player-object fulfills a minimum score requirement
+ * Used to check if a player-object fulfills a minimum score requirement.
  */
 public class ScoreGoal implements GoalInfo {
     private final int minimumPoints;
 
     /**
-     * Assigns a minimum score value
+     * Assigns a minimum score value.
+     *
      * @param minimumPoints minimum allowed score value
      */
-    public ScoreGoal(int minimumPoints){
+    public ScoreGoal(int minimumPoints) {
         this.minimumPoints = minimumPoints;
     }
 
     /**
-     * Checks if a player-object fulfills a minimum score requirement
-     * @param player    player-object to check if fulfills the minimum requirement
-     * @return          True if requirement met, false if not
+     * Checks if a player-object fulfills a minimum score requirement.
+     *
+     * @param player player-object to check if fulfills the minimum requirement
+     * @return True if requirement met, false if not
      */
     @Override
     public boolean isFulfilled(Player player) {
@@ -27,7 +29,8 @@ public class ScoreGoal implements GoalInfo {
     }
 
     /**
-     * Gets the type of the action represented as a string
+     * Gets the type of the action represented as a string.
+     *
      * @return the type of the action represented as a string
      */
     @Override
@@ -37,7 +40,8 @@ public class ScoreGoal implements GoalInfo {
     }
 
     /**
-     * Gets the value associated with the action
+     * Gets the value associated with the action.
+     *
      * @return the value associated with the action
      */
     @Override
