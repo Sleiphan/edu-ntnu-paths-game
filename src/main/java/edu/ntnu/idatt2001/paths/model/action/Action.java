e5d640466117ed6f5ec028e724d21e0c80fb1dce
Sverre <sverre.frogner.haugen@gmail.com>
2023-05-23 09:40:08 +0200

    CheckStyle Action interface

--- src/main/java/edu/ntnu/idatt2001/paths/model/action/Action.java
+++ src/main/java/edu/ntnu/idatt2001/paths/model/action/Action.java
@@ -8,12 +8,14 @@ public interface Action {
 
     /**
      * Performs this action on the specified player.
+     *
      * @param player The player performing the action.
      */
     void execute(Player player);
 
     /**
      * Performs any script-relevant actions on the specified script engine.
+     *
      * @param engine The engine to perform this action on.
      */
     void execute(ScriptEngine engine);
